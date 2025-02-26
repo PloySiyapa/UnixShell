b780bb4820bb3b8b9723f3a7039e5a90dfcc183d
BronsonNguyen <BronsonhuyNguyen@gmail.com>
2025-02-26 09:32:54 -0800

    Merge Task-1 branch, implemeted parsing from Task2

--- wish.c
+++ wish.c
@@ -1,7 +1,35 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAX_TOKENS 64
+
+void executeCommands(char *commands[], int count){
+    //currently prints commands change to Actually execute the commands
+    for (int i = 0; i < count; i++) {
+    printf("Execute Command %d: %s\n", i + 1, commands[i]);
+    }
+}
+
+void parseInput(char line[]){
+    char *commands[MAX_TOKENS]; //array that will store commands
+    char *token; //pointer that sores each command that has been extracted
+    char *rest = line; //pointer that till track the remaining part of string to be parsed
+    int count = 0; //counter to track numer of stored commands
+
+    //strsep() splits the string in half if it finds "&"
+    while((token = strsep(&rest, "&")) != NULL){
+        if(count < MAX_TOKENS){ 
+            commands[count++] = token; //stores the extracted command in the array
+        }
+        else{ //If stop parsing if maximum number of commands is reached
+            break;
+        }
+
+    }
+
+    executeCommands(commands, count);
+}
 
 int main() {
     char *line = NULL; //pointer for storing input
@@ -27,7 +55,7 @@ int main() {
             exit(0);
         }
 
-        printf("you entered: %s \n", line);
+        parseInput(line);
     }
 
 
